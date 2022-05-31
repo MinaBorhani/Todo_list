de963778cc47ccf0da68b7b734fbbbf70a4ecc71
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-05-31 19:31:41 +0430

    commit

--- client/todo-list/src/App.js
+++ client/todo-list/src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello React
+      <form>
+        <input type="text" placeholder="Add Todo Item" />
+        <button type="submit">Add</button>
+      </form>
+      <div className="todo-listItems">
+        <div className="todo-item">
+          <p>This is the item 1</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+        <div className="todo-item">
+          <p>This is the item 2</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+        <div className="todo-item">
+          <p>This is the item 3</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
