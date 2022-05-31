c625aa873ee0dce36c0ba986089c784b0796da29
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-05-31 19:54:29 +0430

    commit

--- client/todo-list/src/App.js
+++ client/todo-list/src/App.js
@@ -11,17 +11,17 @@ function App() {
         <div className="todo-item">
           <p className="item-content">This is the item 1</p>
           <button className="update-item">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
         <div className="todo-item">
           <p className="item-content">This is the item 2</p>
           <button className="update-item">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
         <div className="todo-item">
           <p className="item-content">This is the item 3</p>
           <button className="update-item">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
       </div>
     </div>
