5986821b65187bd1fa35de415675d755aec00d60
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-05-31 18:40:23 +0430

    commit

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -19,7 +19,38 @@ router.post('/api/item', async (req,res)=>{
 })
 
 
+// Lets create  Second route -- get date from datebase
+router.get('/api/item' , async(req,res)=>{
+    try{
+        const allTodoItems = await todoItemsModel.find({});
+        res.status(200).json(allTodoItems)
+    }catch(err){
+        res.json(err);
+    }
+})
 
+
+// Let's update item
+router.put('/api/item/:id' , async(req,res)=>{
+    try{
+        // find the item by id and update it
+        const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id , {$set: req.body});
+        res.status(200).json('Item Updated');
+    }catch(err){
+        res.json(err);
+    }
+})
+
+// Let's Delete item from date base
+router.delete('/api/item/:id' , async (req, res)=>{
+    try{
+        // find the item by its id and delete it
+        const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+        res.status(200).json('Item Deleted');
+    }catch(err) {
+        res.json(err);
+    }
+})
 
 // export router 
 module.exports = router;
