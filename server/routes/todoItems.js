d25abb2a6db62c59068ec00ea7d48d7c8a6d14f5
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-05-31 16:40:14 +0430

    commit

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -1,7 +1,19 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 //import todo model
 const todoItemsModel = require('../models/todoItems');
 
 
 
-//
+// Lest create our first route -- We will add Todo Item to our datebabase
+router.post('/api/item', async (req,res)=>{
+    try{
+        const newItem = new todoItemsModel({
+            item : req.body.item
+        })
+        // save this item datebase
+        const saveItem = await newItem.save()
+        res.status(200).json('Item Added Successfully .')
+    }catch (err){
+        res.json(err);
+    }
+})
